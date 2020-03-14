e6fb94e617d59bde34653ed7b3e13e2b7baabd71
Ansgar Wolsing <ansgar.wolsing@alvary.io>
2020-03-14 20:30:28 +0100

    Update to rki parser (consider German decimal marks)

--- R/parse_rki.R
+++ R/parse_rki.R
@@ -8,7 +8,7 @@ page <- read_html(url)
 # get the first table on the page
 table <- page %>% 
   html_node(css = "table:nth_of_type(1)") %>% 
-  html_table() %>% 
+  html_table(dec = ",") %>% 
   # exclude totals
   filter(Bundesland != "Gesamt")
 
@@ -17,7 +17,7 @@ rki_germany <- table %>%
   select(1, 2, 4) %>% 
   rename(cases = 2, risk_areas = 3) %>% 
   separate(cases, sep = " ", into = c("confirmed_cases", "deaths")) %>% 
-  mutate(confirmed_cases = as.numeric(confirmed_cases),
+  mutate(confirmed_cases = as.numeric(str_replace(confirmed_cases, "\\.", "")),
          deaths = str_replace_all(deaths, "[\\(\\)]", "") %>% as.numeric())
 
 write_rds(rki_germany, "output/rki_germany.RData")
